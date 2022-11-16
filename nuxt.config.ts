037cfa891bf26b38c90b213736f950d2f38dcb33
amitleuva1987 <amitleuva1987@gmail.com>
2022-11-16 18:30:43 +0530

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
+
+    ssr: false,
     css: ["@/assets/css/styles.css"],
     build: {
         transpile: ['@headlessui/vue'],
@@ -17,6 +20,30 @@ export default defineNuxtConfig({
     ],
   
     optimizedImages: {
-      optimizeImages: true
+      inlineImageLimit: 1000,
+      imagesName: ({ isDev }) => isDev ? '[path][name][hash:optimized].[ext]' : 'img/[contenthash:7].[ext]',
+      responsiveImagesName: ({ isDev }) => isDev ? '[path][name]--[width][hash:optimized].[ext]' : 'img/[contenthash:7]-[width].[ext]',
+      handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+      optimizeImages: true,
+      optimizeImagesInDev: true,
+      defaultImageLoader: 'img-loader',
+      mozjpeg: {
+        quality: 80,
+      },
+      optipng: {
+        optimizationLevel: 3,
+      },
+      pngquant: false,
+      gifsicle: {
+        interlaced: true,
+        optimizationLevel: 3,
+      },
+      svgo: {
+        // enable/disable svgo plugins here
+      },
+      webp: {
+        preset: 'default',
+        quality: 75,
+      },
     }
 })
