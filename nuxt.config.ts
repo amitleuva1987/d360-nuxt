066266d1905e7d8901f78c1b6456e5c70191219e
amitleuva1987 <amitleuva1987@gmail.com>
2022-11-17 12:47:58 +0530

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
     app: {
       head: {
-        charset: 'utf-16',
         viewport: 'width=500, initial-scale=1',
         title: 'Discover360 | Instantly Reach Your Future Customers',
         meta: [
