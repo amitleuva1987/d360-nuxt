e3e6c1afc5051ad73a4751d169262055413526a3
amitleuva1987 <amitleuva1987@gmail.com>
2022-11-17 12:50:39 +0530

    disabled meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    app: {
-      head: {
-        viewport: 'width=500, initial-scale=1',
-        title: 'Discover360 | Instantly Reach Your Future Customers',
-        meta: [
-          // <meta name="description" content="My amazing site">
-          { name: 'description', content: 'Make better marketing decisions by learning what ads are already successful. Get comprehensive data on profitable native campaigns.' }
-        ],
-      }
-    },
+    // app: {
+    //   head: {
+    //     viewport: 'width=500, initial-scale=1',
+    //     title: 'Discover360 | Instantly Reach Your Future Customers',
+    //     meta: [
+    //       // <meta name="description" content="My amazing site">
+    //       { name: 'description', content: 'Make better marketing decisions by learning what ads are already successful. Get comprehensive data on profitable native campaigns.' }
+    //     ],
+    //   }
+    // },
     css: ["@/assets/css/styles.css"],
     build: {
         transpile: ['@headlessui/vue'],
